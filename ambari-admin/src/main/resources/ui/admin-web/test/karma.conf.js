b16a89bd64cd712292948e349d86ae4f6ca32181
Akioe Yu <akioeyu@gmail.com>
2024-08-13 19:14:24 +0800

    Update karma.conf.js

--- ambari-admin/src/main/resources/ui/admin-web/test/karma.conf.js
+++ ambari-admin/src/main/resources/ui/admin-web/test/karma.conf.js
@@ -55,7 +55,7 @@ module.exports = function(config){
     customLaunchers: {
       ChromeHeadlessCustom: {
         base: 'ChromeHeadless',
-        flags: ['--no-sandbox', '--disable-gpu', '--disable-translate', '--disable-extensions']
+        flags: ['--no-sandbox', '--disable-gpu', '--disable-translate', '--disable-extensions', '--user-data-dir=/tmp']
       }
     },
 
